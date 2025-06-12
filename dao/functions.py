18059ea9cef5b69a8375356435cd209394a657cc
cerf007 <cerf02062007@gmail.com>
2025-06-11 18:05:41 -0600

    Es donde esta el Class ServiceDao, el cual contiene las funciones para crear las listas y almcenar en ellas

---
+++ dao/functions.py
@@ -0,0 +1,10 @@
+class ServiceDao:
+    def __init__(self):
+        self.service = []
+        
+    def add(self, product):
+        self.service.append(product)
+        
+    def show(self):
+        for product in self.service:
+            print(product)
