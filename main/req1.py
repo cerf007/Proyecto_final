c64de79a4804db0f38e8d5faff38aa3cba414223
cerf007 <cerf02062007@gmail.com>
2025-06-15 09:29:35 -0600

    Se agrego una opcion mas en el print del menu, se agrego ya la función de eliminar cliente, se mejoro el registro del plan

--- main/req1.py
+++ main/req1.py
@@ -8,12 +8,13 @@ def menu():
           Bienvenido a su registradora. ELija de una de las siguientes ipciones
           1.Agregar cliente
           2.Mostrar clientes
-          3.Registrar pagos
-          4.Editar clientes 
-          5.Generar reporte financiero
-          6.Impresión de facturas
-          7.Busqueda de facturas
-          8.Salir
+          3.Editar clientes
+          4.Eliminar Cliente
+          5.Registrar pagos
+          6.Generar reporte financiero
+          7.Impresión de facturas
+          8.Busqueda de facturas
+          9.Salir
           """)
     
 def main():
@@ -25,7 +26,11 @@ def main():
                 registrar_cliente()
             elif opcion == 2:
                 servicios.show()
-            elif opcion == 7:
+            elif opcion == 3:
+                print("Falta este apartado")
+            elif opcion == 4:
+               eliminar_cliente() 
+            elif opcion == 8:
                 buscar_facturas()
             else:
                 print("Opción inválida. Ingrese solo el número 1 por ahora")
@@ -56,13 +61,19 @@ def registrar_cliente():
             plan = int(input("Ingrese el plan elegido por el cliente [1 = Plan básico, 2 = Plan Pro, 3 = Plan Premium]: "))
             
             if 1 <= plan <= 3:
+                if plan == 1:
+                    plan = "Básico"
+                elif plan == 2:
+                    plan = "Pro"
+                elif plan == 3:
+                    plan = "Premium"
                 break
             else:
                 print("Por favor, ingrese un numero del 1 al 3")
         except ValueError:
             print("Carácteres inválidos. Por favor, solo ingrese un numero del 1 al 3")
             
-    registrar = c.Client(nombres, apellidos, numero_telefono, correo_elec, direccion, plan)#Cambiar de tuplas a listas??
+    registrar = c.Client(nombres, apellidos, numero_telefono, correo_elec, direccion, plan)
     servicios.add(registrar)
     
 def buscar_facturas():
@@ -83,3 +94,17 @@ def buscar_facturas():
                    print("Caracter inválido, ingrese solo carácteres alfabéticos")
            except ValueError:
                print("Caracter inválido, ingrese solo carácteres alfabéticos")
+               
+def eliminar_cliente():
+    while True:
+        try:
+            nombre_cliente = input("Ingrese el nombre del cliente a eliminar: ").strip()
+            client_to_delete = next((client for client in servicios.service if client.name == nombre_cliente), None)
+            if client_to_delete:
+                servicios.delete(client_to_delete)
+                print(f"Cliente {nombre_cliente} eliminado.")
+                break
+            else:
+                print("Cliente no encontrado.")
+        except ValueError:
+            print("Entrada no válida, intenté de nuevo")
