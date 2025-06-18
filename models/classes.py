54fea2c1885848871e7f176a18212ad74904f0ad
cerf007 <cerf02062007@gmail.com>
2025-06-18 14:17:54 -0600

    Se agrego el self.identification, una lista para registrar los pagos y se agrego el self.identification en el __str__(self)

--- models/classes.py
+++ models/classes.py
@@ -1,11 +1,15 @@
 class Client:
-    def __init__(self, name, last_name, phone_number, email, address, plan):
+    def __init__(self, name, last_name, identification, phone_number, email, address, plan):
         self.name = name
         self.last_name = last_name
+        self.identification = identification
         self.phone_number = phone_number
         self.email = email
         self.address = address
         self.plan = plan
+        self.pagos = [] #se agreego una lista para registrar los pagos del cliente
         
     def __str__(self):
-        return f"NOMBRE \t| APELLIDO \t| NÚMERO TELEFÓNICO \t| CORREO ELECTRÓNICO \t| DIRECCIÓN \t| PLAN \n{self.name} \t| {self.last_name} \t| {self.phone_number} \t| {self.email} \t| {self.address} \t| {self.plan}"  
+        return f"NOMBRE \t| APELLIDO \t| CÉDULA \t|NÚMERO TELEFÓNICO \t| CORREO ELECTRÓNICO \t| DIRECCIÓN \t| PLAN \n{self.name} \t| {self.last_name} \t| {self.identification}| {self.phone_number} \t| {self.email} \t| {self.address} \t| {self.plan}"  
+    
+    
