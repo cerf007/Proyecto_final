a320346f00d650ad84df41c6d150f7b7c6e7c7e8
cerf007 <cerf02062007@gmail.com>
2025-06-20 07:38:15 -0600

    Se elimino el self.plan del __init__ y del __str__. Se agrego otra función que muestre el formato en el qe debería mostrarse los pagos

--- models/classes.py
+++ models/classes.py
@@ -1,15 +1,19 @@
 class Client:
-    def __init__(self, name, last_name, identification, phone_number, email, address, plan):
+    def __init__(self, name, last_name, identification, phone_number, email, address):
         self.name = name
         self.last_name = last_name
         self.identification = identification
         self.phone_number = phone_number
         self.email = email
         self.address = address
-        self.plan = plan
         self.pagos = [] #se agreego una lista para registrar los pagos del cliente
         
     def __str__(self):
-        return f"NOMBRE \t| APELLIDO \t| CÉDULA \t|NÚMERO TELEFÓNICO \t| CORREO ELECTRÓNICO \t| DIRECCIÓN \t| PLAN \n{self.name} \t| {self.last_name} \t| {self.identification}| {self.phone_number} \t| {self.email} \t| {self.address} \t| {self.plan}"  
+        return f"NOMBRE \t| APELLIDO \t| CÉDULA \t|NÚMERO TELEFÓNICO \t| CORREO ELECTRÓNICO \t| DIRECCIÓN \t|\n{self.name} \t| {self.last_name} \t| {self.identification}| {self.phone_number} \t| {self.email} \t| {self.address} \t"  
+    
+    def display_payments(self):
+        print("Pagos:")
+        for pago in self.pagos:
+            print(f"Pago: {pago['pago']}, Cambio: {pago['cambio']}, Fecha: {pago['fecha']}")
     
     
