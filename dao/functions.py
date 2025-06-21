a9c16eb4e883d5c3250e25308813324fce1b747b
cerf007 <cerf02062007@gmail.com>
2025-06-21 13:08:36 -0600

    Se creó un DAO para almacenar, mostrar y eliminar registro de facturas. Separando la clase ClientDao para que solo guarde los registros del cliente

--- dao/functions.py
+++ dao/functions.py
@@ -16,3 +16,17 @@ class ClientDao:
     def show(self):
         for product in self.service:
              print(product)  # Ahora si imprimirá la representación de cada cliente
+             
+class BillDao:
+    def __init__(self):
+        self.state= []
+        
+    def add(self, bill):
+        self.state.append(bill)
+        
+    def delete(self, bill):
+        self.state.remove(bill)
+        
+    def show(self):
+        for product in self.state:
+             print(product) 
