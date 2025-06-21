cc315802fca83b57dbbf4ac82e1013ea2d49e766
cerf007 <cerf02062007@gmail.com>
2025-06-21 13:09:57 -0600

    Se creo la clase Bill, para tener la propia plantilla dedicada a pagos y facturas de clientes

--- models/classes.py
+++ models/classes.py
@@ -11,9 +11,18 @@ class Client:
     def __str__(self):
         return f"NOMBRE \t| APELLIDO \t| CÉDULA \t|NÚMERO TELEFÓNICO \t| CORREO ELECTRÓNICO \t| DIRECCIÓN \t|\n{self.name} \t| {self.last_name} \t| {self.identification}| {self.phone_number} \t| {self.email} \t| {self.address} \t"  
     
-    def display_payments(self):
-        print("Pagos:")
-        for pago in self.pagos:
-            print(f"Pago: {pago['pago']}, Cambio: {pago['cambio']}, Fecha: {pago['fecha']}")
+    #def display_payments(self):
+    #    print("Pagos:")
+    #    for pago in self.pagos:
+    #        print(f"Pago: {pago['pago']}, Cambio: {pago['cambio']}, Fecha: {pago['fecha']}")
     
-    
+class Bill:
+    def __init__(self, payment, change ,description, date, code):
+        self.payment = payment
+        self.change = change
+        self.description = description
+        self.date =  date
+        self.code = code
+        
+    def __str__(self):
+        return f"|\t NOMBRE: {Client.name} \t|\t APELLIDO: {Client.last_name} \t|\t PAGO: {self.payment} \t|\t CAMBIO: {self.change} \t|\t DESCRIPCIÓN: {self.description} \t|\t FECHA Y HORA: {self.date} \t|\t NUMERO DE FACTURA: {self.code} \t|"
