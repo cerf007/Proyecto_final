14bc4c9d0a6cce909e49499c7f04c411971b602f
cerf007 <cerf02062007@gmail.com>
2025-06-28 18:53:51 -0600

    Se importo pickle y os en el fucntions, creando las funciones correspondientes para cargar, guardar, borrar cambios, actualizar los cambios en una archivos binarios siendo el clientes.bin y el facturas.bin

--- dao/functions.py
+++ dao/functions.py
@@ -1,41 +1,65 @@
+import pickle
+import os
+
 class ClientDao:
     def __init__(self):
-        self.service = []
+        self.file_path = "clientes.bin"
+        self.service = self.load()
         
     def add(self, client):
         self.service.append(client)
+        self.save()
         
     def delete(self, client):
         self.service.remove(client)
+        self.save()
         
     def show(self):
         for product in self.service:
              print(product)  
              
-    def update(self, client, new_data):
-        if 'name' in new_data:
-            client.name = new_data['name']
-        if 'last_name' in new_data:
-            client.last_name = new_data['last_name']
-        if 'identification' in new_data:
-            client.identification = new_data['identification']
-        if 'phone_number' in new_data:
-            client.phone_number = new_data['phone_number']
-        if 'email' in new_data:
-            client.email = new_data['email']
-        if 'address' in new_data:
-            client.address = new_data['address']
+    def update(self, cliente_obj, nuevos_datos):
+        for key, value in nuevos_datos.items():
+            setattr(cliente_obj, key, value)
+        self.save()
              
+    def save(self):
+        with open(self.file_path, "wb") as f:
+            pickle.dump(self.service, f)
+            
+    def load(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "rb") as f:
+                return pickle.load(f)
+        else:
+            return []
+        
+    
+    
 class BillDao:
     def __init__(self):
-        self.state= []
+        self.file_path = "facturas.bin"
+        self.state = self.load()
         
     def add(self, bill):
         self.state.append(bill)
+        self.save()
         
     def delete(self, bill):
         self.state.remove(bill)
+        self.save()
         
     def show(self):
         for product in self.state:
              print(product) 
+             
+    def save(self):
+        with open(self.file_path, "wb") as f:
+            pickle.dump(self.state, f)
+            
+    def load(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, "rb") as f:
+                return pickle.load(f)
+        else:
+            return []
