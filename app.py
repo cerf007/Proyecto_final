2897609e5b0b785736a2e6a4656ce39d4306b58d
cerf007 <cerf02062007@gmail.com>
2025-06-30 19:58:41 -0600

    Se agregaron los comentarios

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import main.req1 as r
 
 r.main()
+
+#main es una carpeta  dentro de mi proyecto
+#req1.py es un archivo Python dentro de esa carpeta
+#El as r indica un alias: en lugar de escribir main.req1 cada vez, puedo abreviar con r
+#Aquí simplemente llamo a la función main() dentro de req1.py para ejecutar todo el programa.
+
